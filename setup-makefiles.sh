2294db86e4f564047ece6540c2747f726648d0b7
C457 <android.c357@gmail.com>
2015-08-28 13:12:15 -0500

    common: allow ms631 to inherit proprietary apps

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -183,7 +183,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter h631,\$(TARGET_DEVICE)),)
+ifneq (\$(filter h631 ms631,\$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
